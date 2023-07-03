72b6898874aa546742d1c6e40060cac4102c716d
musagenius345 <musabadru@gmail.com>
2023-07-03 17:18:23 +0300

    build

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+import postcssPresetEnv from '/data/data/com.termux/files/home/.local/share/pnpm/global/5/.pnpm/postcss-preset-env@9.0.0_postcss@8.4.24/node_modules/postcss-preset-env/dist/index.cjs';
+
+export default {
+  plugins: [
+  postcssPresetEnv({
+      stage: 1
+    })
+  ]
+};
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from '/data/data/com.termux/files/home/.local/share/pnpm/global/5/node_modules/vite'
+
+export default defineConfig({
+  plugins: [],
+})
