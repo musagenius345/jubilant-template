baa814353bf604b8b7c57424d8f04c35716f7fc4
musagenius345 <musabadru@gmail.com>
2023-07-04 05:38:43 +0300

    prettier config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from '/data/data/com.termux/files/home/.local/share/pnpm/global/5/node_modules/vite'
-
+// import stylelint from 'vite-plugin-stylelint'
 export default defineConfig({
-  plugins: [],
+  plugins: []
 })
