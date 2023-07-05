1c71b1c818a51096eccb5dc1d96173df2247c4bf
musagenius345 <musabadru@gmail.com>
2023-07-05 04:50:17 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
-import { defineConfig } from 'vite'
-// import stylelint from 'vite-plugin-stylelint'
+// vite.config.js
+import { defineConfig } from 'vite';
+import path from 'path'; //why?
+
 export default defineConfig({
-  plugins: []
-})
+  plugins: [],
+  resolve: {
+    alias: {
+      '@termux': path.resolve('/data/data/com.termux/files/home/.local/share/pnpm/global/5/node_modules')
+    }
+  }
+});
+
