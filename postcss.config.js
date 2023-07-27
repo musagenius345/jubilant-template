bd1859419f3f44a14ecfca0a76c82cedc7a68aac
musagenius345 <musabadru@gmail.com>
2023-07-27 18:55:35 +0300

    anchors and events

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,5 @@
-import postcssPresetEnv from 'postcss-preset-env';
+// import postcssPresetEnv from '/data/data/com.termux/files/home/.local/share/pnpm/global/5/node_modules/postcss-preset-env/dist/index.mjs';
+import postcssPresetEnv from 'postcss-prest-env';
 
 export default {
   plugins: [
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 // vite.config.js
+// import { defineConfig } from '/data/data/com.termux/files/home/.local/share/pnpm/global/5/node_modules/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [],
-  resolve: {
-    alias: {
-      '@termux': path.resolve('/data/data/com.termux/files/home/.local/share/pnpm/global/5/node_modules')
-    }
-  }
+  plugins: []
 });
 
