ebb63b4d4773ccb8d7c801df78490e1a6a132ca0
musagenius345 <musabadru@gmail.com>
2023-07-29 20:19:11 +0300

    bearable design, carousel, resources

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 // vite.config.js
-// import { defineConfig } from '/data/data/com.termux/files/home/.local/share/pnpm/global/5/node_modules/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from '/data/data/com.termux/files/home/.local/share/pnpm/global/5/node_modules/vite';
+// import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: []
