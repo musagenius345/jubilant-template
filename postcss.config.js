9eb399f43cdcc5fe6f8007477e0cfe7f13c2beb0
musagenius345 <musabadru@gmail.com>
2023-07-30 08:37:46 +0300

    config without termux path

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
-import postcssPresetEnv from '/data/data/com.termux/files/home/.local/share/pnpm/global/5/node_modules/postcss-preset-env/dist/index.mjs';
-// import postcssPresetEnv from 'postcss-prest-env';
+// import postcssPresetEnv from '/data/data/com.termux/files/home/.local/share/pnpm/global/5/node_modules/postcss-preset-env/dist/index.mjs';
+import postcssPresetEnv from 'postcss-prest-env';
 
 export default {
   plugins: [
